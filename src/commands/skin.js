a59ba22b396effd7f5f65fe062b7fdaea12b8f20
_TrueLecter_ <andrew.panassiouk@gmail.com>
2018-08-23 20:11:12 +0300

    Added skin command

---
+++ src/commands/skin.js
@@ -0,0 +1,62 @@
+const { Embeds: EmbedsMode } = require('discord-paginationembed');
+const { MessageEmbed } = require('discord.js');
+const { heroesFuzzy, heroes, translate } = require('../util/cq-data');
+const { getPrefix, textSplitter, capitalizeFirstLetter, imageUrl, parseGrade, parseQuery, statsToString } = require('../util/shared');
+const _ = require('lodash');
+
+const instructions = (message) => {
+    const prefix = getPrefix(message);
+    const e = {
+        title: `${prefix}skin [<name>]`,
+        fields: [{
+                name: '<name>',
+                value: `Get skin data.\n*e.g. ${prefix}skin lee*`,
+            },
+        ],
+    };
+
+    message.channel.send({ embed: e, });
+};
+
+const command = (message, args) => {
+    const grade = parseGrade(args);
+    const name = parseQuery(args, [grade]);
+
+    const candidates = heroesFuzzy.search(name);
+
+    if (!candidates.length) {
+        return message.channel
+            .send('Hero not found!')
+            .catch(error => console.log(error));
+    }
+
+    const hero = heroes[candidates.map(c => parseInt(c.path.split('.')[0]))[0]];
+
+    const embeds = hero.skins.map((skin, idx, arr) => 
+        new MessageEmbed()
+            .setTitle(translate(skin.name))
+            .setDescription(statsToString(skin.stats))
+            .setThumbnail(imageUrl('heroes/' + skin.image))
+            .setFooter(`Page ${idx + 1}/${arr.length}`)
+            .addField('Sell price', skin.cost, true)
+    );
+
+    const msg = new EmbedsMode()
+        .setArray(embeds)
+        .setAuthorizedUsers([message.author.id])
+        .setChannel(message.channel)
+        .showPageIndicator(false)
+        .setDisabledNavigationEmojis(['JUMP'])
+        .build();
+
+    return message.channel
+        .send(msg)
+        .catch(error => console.log(error));
+};
+
+exports.run = (message, args) => {
+    if (!args.length)
+        return instructions(message);
+
+    return command(message, args);
+};
