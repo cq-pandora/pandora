f064fa685d6d44dbe3d165e861dd3f917bba7132
_TrueLecter_ <andrew.panassiouk@gmail.com>
2018-08-23 04:41:58 +0300

    Added sigil command

---
+++ src/commands/sigil.js
@@ -0,0 +1,118 @@
+const { Embeds: EmbedsMode } = require('discord-paginationembed');
+const { MessageEmbed } = require('discord.js');
+const { sigilsFuzzy, sigils, translate, } = require('../util/cq-data');
+const { getPrefix, textSplitter, capitalizeFirstLetter, imageUrl, parseGrade, parseQuery } = require('../util/shared');
+const _ = require('lodash');
+
+const sigilsStatsNameMapping = {
+    "atk_power": "Attack power",
+    "hp": "HP",
+    "crit_chance": "Crit chance",
+    "armor": "Armor",
+    "resistance": "Resistance",
+    "crit_dmg": "Crit damage",
+    "accuracy": "Accuracy",
+    "evasion": "Evasion",
+    "armor_pen": "Armor penetration",
+    "resistance_pen": "Resistance penetration",
+    "dmg_reduction": "Damage reduction",
+    "lifesteal": "Lifesteal",
+    "crit_chance_reduction": "Crit chance reduction",
+}
+
+const statsToString = (obj) => _.entries(obj).map(el => `${sigilsStatsNameMapping[el[0]]}: ${el[1] < 1 ? `${el[1] * 100}%` : el[1]}`).join('\n');
+
+const instructions = (message) => {
+    const prefix = getPrefix(message);
+    const e = {
+//        title: `${prefix}sigil [<name>] [<star>]`,
+        title: `${prefix}sigil [<name>]`,
+        fields: [
+            {
+                name: '<name>',
+                value: `Get sigil data.\n*e.g. ${prefix}sigil refusal*`,
+            },
+            /*{
+                name: '<star>',
+                value: `Filter sigil by <star>.\n*e.g. ${prefix}sigil refusal 1*`,
+            },*/ 
+        ],
+    };
+
+    message.channel.send({
+        embed: e,
+    });
+};
+
+const command = (message, args) => {
+    const grade = parseGrade(args);
+    const name = parseQuery(args, [grade]);
+
+    const candidates = sigilsFuzzy.search(name);
+
+    if (!candidates.length) {
+        return message.channel
+            .send('Sigil not found!')
+            .catch(error => console.log(error));
+    }
+
+    const sigil = grade ? candidates.map(c => sigils[c.path]).filter(b => b.grade === grade)[0]
+                        : sigils[candidates[0].path]
+
+    if (!sigil) 
+        return message.channel
+            .send('Sigil grade not found!')
+            .catch(error => console.log(error));
+
+    let embed = new MessageEmbed()
+        .setDescription(translate(sigil.description))
+        .setTitle(`${translate(sigil.name)} (${sigil.grade}★)`)
+        .setThumbnail(imageUrl('sigils/' + sigil.image))
+        .addField('Rarity', capitalizeFirstLetter(sigil.rarity), true)
+        .addField('Sell cost', sigil.sell_cost, true)
+        .addField('Extract cost', sigil.extract_cost, true);
+
+    let stats = {}, totalStats = {};
+
+    for (const key in sigil.stats) {
+        if (!sigil.stats[key])
+            continue;
+
+        stats[key] = sigil.stats[key];
+        totalStats[key] = sigil.stats[key];   
+    }
+
+    embed = embed.addField('Stats', statsToString(stats), true);
+
+    if (sigil.set) {
+        const otherPiece = sigils.filter(s => s.ingame_id === sigil.set.pair)[0];
+        let otherStats = {};
+
+        for (const key in otherPiece.stats) {
+            if (!otherPiece.stats[key])
+                continue;
+            
+            if (!totalStats[key]) totalStats[key] = 0;
+            totalStats[key] += otherPiece.stats[key];  
+            otherStats[key] = otherPiece.stats[key];
+        }
+
+        // FIXME somehow parse total stats with set effect
+
+        ember = embed
+            .addField('Other piece stats', statsToString(otherStats), true)
+            .addField('Set effect', translate(sigil.set.effect), true)
+            .setFooter(`Set: ${translate(sigil.set.name)}`);
+    }
+
+    return message.channel
+        .send(embed)
+        .catch(error => console.log(error));
+};
+
+exports.run = (message, args) => {
+    if (!args.length) 
+        return instructions(message);
+
+    return command(message, args);
+};
