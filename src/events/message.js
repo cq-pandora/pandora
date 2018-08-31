7fb70c8a840cd5d1db5ed08a2cc74381ebb1a0f4
_TrueLecter_ <andrew.panassiouk@gmail.com>
2018-08-31 18:01:11 +0300

    Empty arguments are now skipped

--- src/events/message.js
+++ src/events/message.js
@@ -1,56 +1,72 @@
 const config = require('../config');
+const fs = require('fs');
+const _ = require('lodash');
+const path = require('path');
+
+const commandsDir = '../commands/';
+
+const commands = _.reduce(fs.readdirSync(path.resolve(__dirname, commandsDir)), (res, c) => {
+    if (!c.endsWith('.js')) {
+        return res;
+    }
+
+    res[c.substring(0, c.length - 3).toLowerCase()] = require(path.resolve(__dirname, commandsDir, c));
+    return res;
+}, {});
 
 module.exports = message => {
-  // uncomment for self bot
-  // if (message.author.id !== message.client.user.id) {
-  //   return;
-  // }
+    // uncomment for self bot
+    // if (message.author.id !== message.client.user.id) {
+    //   return;
+    // }
 
-  // ignore bot messages
-  if (message.author.bot) {
-    return;
-  }
+    // ignore bot messages
+    if (message.author.bot) {
+        return;
+    }
 
-  // ignore messages not from guild text channels
-  // if (message.channel.type !== 'text') {
-  //   return;
-  // }
+    // ignore messages not from guild text channels
+    // if (message.channel.type !== 'text') {
+    //   return;
+    // }
 
-  const mentionRegExp = RegExp(`^<@!?${message.client.user.id}>`);
-  const noPrefix = !config.prefix || !message.content.startsWith(config.prefix);
-  const noMention = !mentionRegExp.test(message.content);
+    const mentionRegExp = RegExp(`^<@!?${message.client.user.id}>`);
+    const noPrefix = !config.prefix || !message.content.startsWith(config.prefix);
+    const noMention = !mentionRegExp.test(message.content);
 
-  // ignore if not a command
-  if (noPrefix && noMention) {
-    return;
-  }
+    // ignore if not a command
+    if (noPrefix && noMention) {
+        return;
+    }
 
-  // parse message into command and arguments
-  let args;
-  let command;
-  if (noMention) {
-    args = message.content.split(' ');
-    command = args
-      .shift()
-      .slice(config.prefix.length)
-      .toLowerCase();
-  } else {
-    args = message.content
-      .replace(mentionRegExp, '')
-      .trim()
-      .split(' ');
-    command = args.shift().toLowerCase();
-  }
+    // parse message into command and arguments
+    let args;
+    let command;
+    if (noMention) {
+        args = message.content.split(' ').filter(t => !!t);
+        command = args
+            .shift()
+            .slice(config.prefix.length)
+            .toLowerCase();
+    } else {
+        args = message.content
+            .replace(mentionRegExp, '')
+            .trim()
+            .split(' ')
+            .filter(t => !!t);
+        command = args.shift().toLowerCase();
+    }
 
-  // check if command file exists
-  try {
-    console.log(
-      (message.channel.type === 'text'
-        ? `${message.guild.name}#${message.channel.name}|`
-        : '') + `${message.author.tag}: ${message.content}`
-    );
-    require(`../commands/${command}`).run(message, args);
-  } catch (error) {
-    console.log(error);
-  }
+    if (!(commands[command] || commands[config.aliases[command]])){
+    	//message.channel.send('Command not found!');
+    	return;
+    }
+
+    // check if command file exists
+    try {
+        (commands[command] || commands[config.aliases[command]]).run(message, args);
+    } catch (error) {
+    	message.channel.send('Error while executing command!');
+        console.log(error);
+    }
 };
