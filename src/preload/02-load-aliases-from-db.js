5f81c7f6466f9fb54913f6ae5b4d44b2c896050e
_TrueLecter_ <andrew.panassiouk@gmail.com>
2018-09-02 05:54:11 +0300

    Fixed aliases not being loaded

--- src/preload/02-load-aliases-from-db.js
+++ src/preload/02-load-aliases-from-db.js
@@ -4,6 +4,6 @@ const _ = require('lodash');
 const compare = require('compare-versions');
 
 module.exports = () => aliases.get()
-	.then(ts => _.reduce(ts, (r, t) => (r[t.alias] = t.for), config.aliases));
+	.then(ts => _.reduce(ts, (r, t) => (r[t.alias] = t.for, r), config.aliases));
 
 module.exports.errorCode = 3;
