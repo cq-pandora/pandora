fee0625bdf549e5d96ee339c4a8f19c1b24d302b
_TrueLecter_ <andrew.panassiouk@gmail.com>
2018-09-17 21:09:03 +0300

    Ignoring discord-paginationembed error on deleting message

--- src/events/unhandledRejection.js
+++ src/events/unhandledRejection.js
@@ -1,3 +1,5 @@
 module.exports = error => {
+    // Ignore discord api errors related to embed
+    if (error && error.name === 'DiscordAPIError' && error.message === 'Unknown Message') return;
     console.log(error);
 };
