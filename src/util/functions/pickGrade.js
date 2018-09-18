225c7f0ac8fa308a96234abf4cb1b19fdc7426bb
_TrueLecter_ <andrew.panassiouk@gmail.com>
2018-09-18 23:44:07 +0300

    pickGrade function to pick 3-6 garde with said rates

---
+++ src/util/functions/pickGrade.js
@@ -0,0 +1,11 @@
+module.exports = () => {
+    const roll = Math.random();
+    if (roll >= 0 && roll <= 0.81) {
+        return '3';
+    } else if (roll > 0.81 && roll <= 0.81 + 0.149) {
+        return '4';
+    } else if (roll > 0.81 + 0.149 && roll <= 0.81 + 0.149 + 0.035) {
+        return '5';
+    }
+    return '6';
+};
