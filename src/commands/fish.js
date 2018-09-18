765ddfec6b3e1248d2e3c4a1720a1ec6ba835e96
_TrueLecter_ <andrew.panassiouk@gmail.com>
2018-09-18 03:13:33 +0300

    Moved embeds from !fish command

--- src/commands/fish.js
+++ src/commands/fish.js
@@ -1,12 +1,12 @@
-const { MessageEmbed } = require('discord.js');
-const { emojis } = require('../config');
 const {
-    fileDb: { fishesFuzzy, followPath, translate },
-    functions: { getPrefix, capitalizeFirstLetter, imageUrl, parseQuery },
+    fileDb: { fishesFuzzy, followPath },
+    functions: { getPrefix, parseQuery },
     categories,
     cmdResult,
     PaginationEmbed,
 } = require('../util');
+
+const FishListEmbed = require('../embeds/FishListEmbed');
 
 const instructions = (message) => {
     const prefix = getPrefix(message);
@@ -39,35 +39,9 @@ const command = (message, args) => {
             }));
     }
 
-    const rewards = {
-        ITEM_FISHCOIN: emojis.fishcoin,
-        ITEM_GOLD: emojis.gold,
-        RANDOMBOX_EVENT_GOOD_CATBOX: emojis.mossy_cat_chest,
-        RANDOMBOX_EVENT_NORMAL_CATBOX: emojis.intact_cat_chest,
-    };
+    const fishes = candidates.map(c => followPath(c.path));
 
-    const fishes = candidates.map((c, idx, arr) => {
-        const fish = followPath(c.path);
-        return new MessageEmbed()
-            .setTitle(`${translate(fish.name)} (${fish.grade}★)`)
-            .setDescription(translate(fish.description))
-            .addField('Exp', fish.exp, true)
-            .addField('Area type', capitalizeFirstLetter(fish.habitat), true)
-            .addField('Initial range', `${fish.starts_from}m`, true)
-            .addField(`Reward${fish.rewards.length > 1 ? 's' : ''}`,
-                fish.rewards.map(r => `${r.amount > 1 ? r.amount : ''} ${rewards[r.type]}`).join('\n'),
-                true
-            )
-            .setFooter(`Page ${idx + 1}/${arr.length}`)
-            .setThumbnail(imageUrl('fish/' + fish.image));
-    });
-
-    return new PaginationEmbed(message)
-        .setArray(fishes)
-        .setAuthorizedUsers([message.author.id])
-        .setChannel(message.channel)
-        .showPageIndicator(false)
-        .build()
+    return new FishListEmbed(message, fishes).send()
         .then(m => ({
             status_code: cmdResult.SUCCESS,
             target: fishes.map(f => f.id).join(','),
---
+++ src/embeds/FishListEmbed.js
@@ -0,0 +1,38 @@
+const PaginationEmbed = require('./PaginationEmbed');
+const { MessageEmbed } = require('discord.js');
+const { emojis } = require('../config');
+const {
+    functions: { capitalizeFirstLetter, imageUrl },
+    fileDb: { translate, followPath },
+} = require('../util');
+
+const rewards = {
+    ITEM_FISHCOIN: emojis.fishcoin,
+    ITEM_GOLD: emojis.gold,
+    RANDOMBOX_EVENT_GOOD_CATBOX: emojis.mossy_cat_chest,
+    RANDOMBOX_EVENT_NORMAL_CATBOX: emojis.intact_cat_chest,
+};
+
+class FishListEmbed extends PaginationEmbed {
+    constructor (initialMessage, fishes) {
+        super(initialMessage);
+
+        const embeds = fishes.map(fish =>
+            new MessageEmbed()
+                .setTitle(`${translate(fish.name)} (${fish.grade}★)`)
+                .setDescription(translate(fish.description))
+                .addField('Exp', fish.exp, true)
+                .addField('Area type', capitalizeFirstLetter(fish.habitat), true)
+                .addField('Initial range', `${fish.starts_from}m`, true)
+                .addField(`Reward${fish.rewards.length > 1 ? 's' : ''}`,
+                    fish.rewards.map(r => `${r.amount > 1 ? r.amount : ''} ${rewards[r.type]}`).join('\n'),
+                    true
+                )
+                .setThumbnail(imageUrl('fish/' + fish.image))
+        );
+
+        this.setArray(embeds).showPageIndicator(false);
+    }
+}
+
+module.exports = FishListEmbed;
