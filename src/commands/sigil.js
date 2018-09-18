d7bac571ccfad8aba10abc7500141329881b9b8d
_TrueLecter_ <andrew.panassiouk@gmail.com>
2018-09-18 13:55:11 +0300

    Extracted embed from !sigil command

--- src/commands/sigil.js
+++ src/commands/sigil.js
@@ -1,25 +1,20 @@
-const { MessageEmbed } = require('discord.js');
 const {
-    fileDb: { sigilsFuzzy, followPath, sigils, translate },
-    functions: { getPrefix, capitalizeFirstLetter, imageUrl, parseGrade, parseQuery, statsToString },
+    fileDb: { sigilsFuzzy, followPath },
+    functions: { getPrefix, parseQuery },
     categories,
     cmdResult,
 } = require('../util');
+const SigilsListEmbed = require('../embeds/SigilsListEmbed');
 
 const instructions = (message) => {
     const prefix = getPrefix(message);
     const e = {
-        //        title: `${prefix}sigil [<name>] [<star>]`,
-        title: `${prefix}sigil [<name>]`,
+        title: `${prefix}sigil <name>`,
         fields: [
             {
                 name: '<name>',
                 value: `Get sigil data.\n*e.g. ${prefix}sigil refusal*`
             }
-            /* {
-                name: '<star>',
-                value: `Filter sigil by <star>.\n*e.g. ${prefix}sigil refusal 1*`,
-            }, */
         ]
     };
 
@@ -30,8 +25,7 @@ const instructions = (message) => {
 };
 
 const command = (message, args) => {
-    const grade = parseGrade(args);
-    const name = parseQuery(args, [`${grade}`]);
+    const name = parseQuery(args);
 
     const candidates = sigilsFuzzy.search(name);
 
@@ -44,62 +38,9 @@ const command = (message, args) => {
             }));
     }
 
-    const sigil = grade ? candidates.map(c => followPath(c.path)).filter(b => b.grade === grade)[0]
-        : followPath(candidates[0].path);
+    const sigils = candidates.map(c => followPath(c.path));
 
-    if (!sigil) {
-        return message.channel
-            .send('Sigil grade not found!')
-            .then(m => ({
-                status_code: cmdResult.ENTITY_GRADE_NOT_FOUND,
-                target: sigil.id,
-            }));
-    }
-
-    let embed = new MessageEmbed()
-        .setDescription(translate(sigil.description))
-        .setTitle(`${translate(sigil.name)} (${sigil.grade}★)`)
-        .setThumbnail(imageUrl('sigils/' + sigil.image));
-
-    let stats = {}; let totalStats = {};
-
-    for (const key in sigil.stats) {
-        if (!sigil.stats[key]) { continue; }
-
-        stats[key] = sigil.stats[key];
-        totalStats[key] = sigil.stats[key];
-    }
-
-    embed = embed.addField('Stats', statsToString(stats), true);
-
-    if (sigil.set) {
-        const otherPiece = sigils.filter(s => s.ingame_id === sigil.set.pair)[0];
-        let otherStats = {};
-
-        for (const key in otherPiece.stats) {
-            if (!otherPiece.stats[key]) { continue; }
-
-            if (!totalStats[key]) totalStats[key] = 0;
-            totalStats[key] += otherPiece.stats[key];
-            otherStats[key] = otherPiece.stats[key];
-        }
-
-        // FIXME somehow parse total stats with set effect
-
-        embed = embed
-            .addField('Set effect', translate(sigil.set.effect), true)
-            .addField('Other piece', translate(otherPiece.name), true)
-            .addField('Other piece stats', statsToString(otherStats), true)
-            .setFooter(`Set: ${translate(sigil.set.name)}`);
-    }
-
-    return message.channel
-        .send(embed
-            .addBlankField()
-            .addField('Sell price', sigil.sell_cost, true)
-            .addField('Extract cost', sigil.extract_cost, true)
-            .addField('Rarity', capitalizeFirstLetter(sigil.rarity), true)
-        )
+    return new SigilsListEmbed(message, sigils).send()
         .then(m => ({
             status_code: cmdResult.SUCCESS,
             target: sigil.id,
---
+++ src/embeds/SigilsListEmbed.js
@@ -0,0 +1,61 @@
+const PaginationEmbed = require('./PaginationEmbed');
+const { MessageEmbed } = require('discord.js');
+const { emojis } = require('../config');
+const {
+    functions: { statsToString, imageUrl, capitalizeFirstLetter },
+    fileDb: { translate, sigils:sigilz },
+} = require('../util');
+
+class SigilsListEmbed extends PaginationEmbed {
+    constructor (initialMessage, sigils) {
+        super(initialMessage);
+
+        const embeds = sigils.map(sigil => {
+            let embed = new MessageEmbed()
+                .setDescription(translate(sigil.description))
+                .setTitle(`${translate(sigil.name)} (${sigil.grade}★)`)
+                .setThumbnail(imageUrl('sigils/' + sigil.image));
+
+            let stats = {}; let totalStats = {};
+
+            for (const key in sigil.stats) {
+                if (!sigil.stats[key]) { continue; }
+
+                stats[key] = sigil.stats[key];
+                totalStats[key] = sigil.stats[key];
+            }
+
+            embed = embed.addField('Stats', statsToString(stats), true);
+
+            if (sigil.set) {
+                const otherPiece = sigilz.filter(s => s.ingame_id === sigil.set.pair)[0];
+                let otherStats = {};
+
+                for (const key in otherPiece.stats) {
+                    if (!otherPiece.stats[key]) { continue; }
+
+                    if (!totalStats[key]) totalStats[key] = 0;
+                    totalStats[key] += otherPiece.stats[key];
+                    otherStats[key] = otherPiece.stats[key];
+                }
+
+                // FIXME somehow parse total stats with set effect
+
+                embed = embed
+                    .addField('Set effect', translate(sigil.set.effect), true)
+                    .addField('Other piece', translate(otherPiece.name), true)
+                    .addField('Other piece stats', statsToString(otherStats), true)
+                    .setFooter(`Set: ${translate(sigil.set.name)}`);
+            }
+
+            return embed.addBlankField()
+                .addField('Sell price', `${sigil.sell_cost}${emojis.gold}`, true)
+                .addField('Extract cost', `${sigil.extract_cost}${emojis.gold}`, true)
+                .addField('Rarity', capitalizeFirstLetter(sigil.rarity), true);
+        });
+
+        this.setArray(embeds).showPageIndicator(false);
+    }
+}
+
+module.exports = SigilsListEmbed;
