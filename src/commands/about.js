cc318d4bd502ca602e8c0c9c345db611690840d6
_TrueLecter_ <andrew.panassiouk@gmail.com>
2018-09-21 21:04:21 +0300

    Added about command and updated LICENSE

---
+++ src/commands/about.js
@@ -0,0 +1,29 @@
+const config = require('../config');
+const { cmdResult } = require('../util');
+
+exports.run = (message) => message.channel.send({
+    embed: {
+        title: 'About',
+        description: `Made with ❤ by ${message.guild.members.get(config.owner_id)} (${message.guild.members.get(config.owner_id).user.tag}).` + '\n\nThis bot is not affiliated, associated, authorized by, endorsed by, or in any way officially connected with NHN Entertainment Corp., LoadComplete Inc., or any of their subsidiaries or their affiliates.',
+        fields: [
+            {
+                name: `Invite ${message.client.user.username}`,
+                value: '[bit.ly/InvitePandora](http://bit.ly/InvitePandora)',
+                inline: true,
+            },
+            {
+                name: 'Join Servers',
+                value: '[Pandora Dev Server](https://discord.gg/pK9qsJY)\n[Crusaders Quest](https://discord.gg/6TRnyhj)',
+                inline: true,
+            },
+            {
+                name: 'Git repos',
+                value: '[/TrueLecter/pandora](https://github.com/TrueLecter/pandora)\n[cq-assets](https://gitlab.com/cq-data-re/cq-assets)',
+                inline: true,
+            },
+        ],
+    },
+}).then(m => ({
+    target: 'about',
+    status_code: cmdResult.SUCCESS,
+}));
