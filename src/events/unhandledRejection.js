53dc02d18d16b36769a35957ec4e259ea32dd5f9
_TrueLecter_ <andrew.panassiouk@gmail.com>
2018-09-21 08:46:30 +0300

    Ignoring missing permissions now

--- src/events/unhandledRejection.js
+++ src/events/unhandledRejection.js
@@ -1,5 +1,5 @@
 module.exports = error => {
     // Ignore discord api errors related to embed
-    if (error && error.name === 'DiscordAPIError' && error.message === 'Unknown Message') return;
+    if (error && error.name === 'DiscordAPIError' && (error.message === 'Unknown Message' || error.message === 'Missing Permissions')) return;
     console.log(error);
 };
