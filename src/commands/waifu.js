3067c633c59602c96b24c5690e4c0f4c88b6969e
_TrueLecter_ <andrew.panassiouk@gmail.com>
2018-09-24 00:01:26 +0300

    Added !waifu command to show random sprite

---
+++ src/commands/waifu.js
@@ -0,0 +1,30 @@
+const { random, imageUrl } = require('../functions');
+const {
+    categories,
+    cmdResult,
+    fileDb: {
+        heroes,
+    }
+} = require('../util');
+
+exports.run = async (message) => {
+    const hero = heroes[random(0, heroes.length - 1)];
+
+    await message.channel.send({
+       embed: {
+           image: {
+               url: imageUrl(`heroes/${hero.forms[hero.forms.length - 1].image}`),
+           },
+           footer: {
+               text: `${message.author.username}#${message.author.discriminator}`,
+           }
+       }
+    });
+
+    return {
+        status_code: cmdResult.SUCCESS,
+        target: 'waifu',
+    };
+};
+
+exports.category = categories.MISC;
