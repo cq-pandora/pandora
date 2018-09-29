3b0c66b2c614636db6eed11b3a7c1f37201a9696
_TrueLecter_ <andrew.panassiouk@gmail.com>
2018-09-29 20:02:10 +0300

    Unhandled rejection message and added two factor errors ingore

--- src/events/unhandledRejection.js
+++ src/events/unhandledRejection.js
@@ -5,13 +5,18 @@ const logger = require('../logger');
 const ERROR_CODES = [
 	APIErrors.UNKNOWN_MESSAGE,
 	APIErrors.MISSING_PERMISSIONS,
+    APIErrors.CANNOT_EXECUTE_ON_DM,
+];
+
+const IGNORE_RAW = [
+	'Two factor is required for this operation',
 ];
 
 module.exports = (error) => {
 	// Ignore discord api errors related to embed
-	if (error instanceof DiscordAPIError && ERROR_CODES.includes(error.code)) {
+	if (error instanceof DiscordAPIError && (ERROR_CODES.includes(error.code) || IGNORE_RAW.includes(error.message))) {
 		return;
 	}
 
-	logger.error(error);
+	logger.error('Unhandled exception!', error);
 };
