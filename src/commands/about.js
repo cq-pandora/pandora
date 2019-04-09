7c6fcc5b865300c646a42df8bc3c7fede6791a4a
_TrueLecter_ <andrew.panassiouk@gmail.com>
2019-04-09 17:53:19 +0300

    Fixed about

--- src/commands/about.js
+++ src/commands/about.js
@@ -1,11 +1,11 @@
 const config = require('../config');
 const { cmdResult, categories } = require('../util');
 
-exports.run = async ({ message }) => {
+exports.run = async ({ client, message }) => {
 	await message.channel.send({
 		embed: {
 			title: 'About',
-			description: `Made with ❤ by ${message.guild.members.get(config.owner_id)} (${message.guild.members.get(config.owner_id).user.tag}).\n\nThis bot is not affiliated, associated, authorized by, endorsed by, or in any way officially connected with NHN Entertainment Corp., LoadComplete Inc., or any of their subsidiaries or their affiliates.`,
+			description: `Made with ❤ by ${client.users.get(config.owner_id)} (${message.guild.members.get(config.owner_id).user.tag}).\n\nThis bot is not affiliated, associated, authorized by, endorsed by, or in any way officially connected with NHN Entertainment Corp., LoadComplete Inc., or any of their subsidiaries or their affiliates.`,
 			fields: [
 				{
 					name: `Invite ${message.client.user.username}`,
