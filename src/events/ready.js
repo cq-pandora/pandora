002e0a9c2069086fdb5227dc176b1877b2e22bca
_TrueLecter_ <andrew.panassiouk@gmail.com>
2019-04-09 17:53:44 +0300

    Added latest feature in status

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,4 +2,5 @@ const logger = require('../logger');
 
 module.exports = (client) => {
 	logger.info(`Logged in as ${client.user.tag}`);
+	client.user.setActivity('working portraits', { type: 'WATCHING' });
 };
